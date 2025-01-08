5183509c111defa50caaf6d683fa438897d4a671
Reza Fardan <rezafardaaan@gmail.com>
2025-01-08 23:01:04 +0700

    change next config to async rewrites

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "/:path*",
+      },
+    ];
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
